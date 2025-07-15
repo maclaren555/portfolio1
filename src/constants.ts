dc5c8863d4f072e223fb60f7d603ee65f8e7246d
maclaren555 <macbook@macbooks-MacBook-Pro.local>
2025-07-15 18:50:16 +0300

    Fix: correct video paths for deployment

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const VIDEO_LINKS = {
-	mclaren: '/videos/mclaren.mp4',
-	dota: '/videos/dota.mp4',
-	code: '/videos/coding.mp4',
+    mclaren: 'videos/mclaren.mp4',
+    dota: 'videos/dota.mp4',
+    code: 'videos/coding.mp4',
 };
 
 export const GOLDEN_RATIO = 1.618; 
